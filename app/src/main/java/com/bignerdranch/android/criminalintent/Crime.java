f28003eeea0f9b62e84e3d67b0ec68ebeb8d37ca
jSonKang <ikang3@binghamton.edu>
2017-03-25 08:59:07 -0400

    Initial commit - Chapter 7(Fragments)

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,57 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by jSonKang on 3/19/17.
+ */
+
+public class Crime
+{
+    private UUID mID;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public UUID getID()
+    {
+        return mID;
+    }
+
+    public Date getDate()
+    {
+        return mDate;
+    }
+
+    public boolean isSolved()
+    {
+        return mSolved;
+    }
+
+    public void setTitle(String title)
+    {
+        mTitle = title;
+    }
+
+    public void setDate(Date date)
+    {
+        mDate = date;
+    }
+
+    public void setSolved(boolean solved)
+    {
+        mSolved = solved;
+    }
+
+    public String getTitle()
+    {
+        return mTitle;
+    }
+
+    public Crime()
+    {
+        mID = UUID.randomUUID();
+        mDate = new Date();
+    }
+}
